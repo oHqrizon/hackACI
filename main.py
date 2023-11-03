3d9168535a0891693e67567af601a51e47af3565
Jackson Cai <jacksoncai200@gmaill.com>
2023-11-03 17:41:23 -0400

    flask setup running

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>testing</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
