9361f62c7f17e48803fda3be2c4c35ab04626963
Jackson Cai <jacksoncai200@gmaill.com>
2023-11-04 14:40:48 -0400

    started home page, along with animations

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
