9543068edc6702cb87fb7da7a1e928a6b2932b36
Jackson Cai <jacksoncai200@gmaill.com>
2023-11-08 18:01:40 -0500

    completed registeration front end, along with link connections

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
