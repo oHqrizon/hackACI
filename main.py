beaad9e9745c10d6ac59eab2f4420fc93b3ec5d0
Jackson Cai <jacksoncai200@gmaill.com>
2023-11-20 22:32:46 -0500

    update gitignore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
-import flask_sqlalchemy
-from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 
 conn = sqlite3.connect('users.db')
